c1e8c216bb5740695e4322d47fa608349911634a
Oscar Olsson <happybits@gmail.com>
2022-08-04 12:48:50 +0200

    -

---
+++ Frontend/Components/Lab/01.js
@@ -0,0 +1,20 @@
+
+    const area = document.querySelector(".area")
+
+    function hideArea() {
+        area.classList.add("hideMe")
+    }
+
+    function showArea() {
+        area.classList.remove("hideMe")
+    }
+
+    function changeText() {
+        area.innerHTML = "Ny text!"
+    }
+
+    function pow() {
+        area.classList.add("standout")
+    }
+
+
