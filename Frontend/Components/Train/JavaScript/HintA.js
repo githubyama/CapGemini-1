cb3a82e741171e9600f26dbfc76ec9f79ab4fc6c
Oscar Olsson <happybits@gmail.com>
2022-09-19 16:26:44 +0200

    -

---
+++ Frontend/Components/Train/JavaScript/HintA.js
@@ -0,0 +1,16 @@
+const result = document.querySelector(".train .result")
+const from = document.querySelector(".train .from")
+const to = document.querySelector(".train .to")
+const okbutton = document.querySelector(".train button")
+
+function update() {
+    console.log("update")
+}
+
+function wayChange(way){
+    console.log(`You want to change to "${way}"`)
+}
+
+function makeChoice() {
+    console.log("Make choice")
+}
