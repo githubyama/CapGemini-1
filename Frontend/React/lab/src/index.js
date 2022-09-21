414ff9d1769833bf8ffa3a601ee0f34d8a392d94
Oscar Olsson <happybits@gmail.com>
2022-09-21 16:49:45 +0200

    -

---
+++ Frontend/React/lab/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Email from './email/Email01';
+
+ReactDOM.render(
+
+<div>
+    <h1>My header!</h1>
+    <Email/>
+
+    <a href="google.com">Link to my fav site</a>
+    <Email/>
+</div>
+
+, document.getElementById("root"))
