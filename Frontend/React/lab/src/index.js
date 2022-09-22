be2e2d7ff3bbc813bc28ca69816f30ce045d1d63
Oscar Olsson <happybits@gmail.com>
2022-09-22 09:35:25 +0200

    -

--- Frontend/React/lab/src/index.js
+++ Frontend/React/lab/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Email from './email/Email01';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Email from './email/Email01';
+// ReactDOM.render(
 
-ReactDOM.render(
+// <div>
+//     <h1>My header!</h1>
+//     <Email/>
 
-<div>
-    <h1>My header!</h1>
-    <Email/>
+//     <a href="google.com">Link to my fav site</a>
+//     <Email/>
+// </div>
 
-    <a href="google.com">Link to my fav site</a>
-    <Email/>
-</div>
+// , document.getElementById("root"))
 
-, document.getElementById("root"))
+import Email from './email/Email02';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(
 
+    <div>
+        <h1>My header!</h1>
+        <Email/>
 
+        <a href="google.com">Link to my fav site</a>
+        <Email/>
+    </div>
+
+);
