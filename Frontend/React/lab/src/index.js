1d47622efa83920984cf9f39d61dff2a053b5233
Oscar Olsson <happybits@gmail.com>
2022-09-22 15:23:26 +0200

    -

--- Frontend/React/lab/src/index.js
+++ Frontend/React/lab/src/index.js
@@ -1,19 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Email from './email/Email01';
-// ReactDOM.render(
 
-// <div>
-//     <h1>My header!</h1>
-//     <Email/>
-
-//     <a href="google.com">Link to my fav site</a>
-//     <Email/>
-// </div>
-
-// , document.getElementById("root"))
-
-import Email from './email/Email05';
+import Email from './email/Email07';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container); 
@@ -21,10 +7,11 @@ root.render(
 
     <div>
         <h1>My header!</h1>
-        <Email/>
+        <Email success-message="Good boy!" background="burlywood" />
+        <Email success-message="Whatever..." background="aqua" border-thickness={20} />
 
         <a href="google.com">Link to my fav site</a>
-        <Email/>
+        <Email />
     </div>
 
 );
